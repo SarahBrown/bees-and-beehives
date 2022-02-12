ef0a457248e5deac70f357a745024b1262c5fdaf
Sarah Brown <srb@ou.edu>
2022-02-12 13:08:56 -0600

    basics

---
+++ game.js
@@ -0,0 +1,20 @@
+// round time limit
+let TIME_LIMIT = 60;
+
+// quotes, https://honeybeenet.gsfc.nasa.gov/Honeybees/Basics.htm
+let quotes = [
+    "Bees have 5 eyes",
+    "Male bees in the hive are called drones",
+    "An average beehive can hold around 50,000 bees",
+    "Average per capita honey consumption in the US is 1.3 pounds",
+    "Bees carry pollen on their hind legs in a pollen basket or corbicula"
+]
+
+
+function startGame() {
+
+}
+
+function finishGame() {
+    
+}
