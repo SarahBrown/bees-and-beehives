60e6412ba620237ea98738c7708be8094d668f67
Sarah Brown <srb@ou.edu>
2022-02-12 21:52:53 -0600

    update

--- scripts/game.js
+++ scripts/game.js
@@ -52,6 +52,7 @@ let quotes6 = [
 
 let numQuotes = 5;
 
+let curLevel = $(".cur-level").text();
 let timerText = $(".cur-time");
 let accuracyText = $(".cur-accuracy");
 let errorText = $(".cur-errors");
@@ -74,18 +75,26 @@ let quoteNum = 0;
 let timer = null;
 
 function updateQuote() {
-    quoteText.textContent = null;
-    switch(levelGroup){
-        case 1: curQuote = quotes1[quoteNum]; break;
-        case 2: curQuote = quotes2[quoteNum]; break;
-        case 3: curQuote = quotes3[quoteNum]; break;
-        case 4: curQuote = quotes4[quoteNum]; break;
-        case 5: curQuote = quotes5[quoteNum]; break;
-        case 6: curQuote = quotes6[quoteNum]; break;
+    quoteText.text(null);
+
+    // switches between quotes array depending on level
+    switch(curLevel){
+        case "1": curQuote = quotes1[quoteNum]; break;
+        case "2": curQuote = quotes2[quoteNum]; break;
+        case "3": curQuote = quotes3[quoteNum]; break;
+        case "4": curQuote = quotes4[quoteNum]; break;
+        case "5": curQuote = quotes5[quoteNum]; break;
+        case "6": curQuote = quotes6[quoteNum]; break;
     }
 
-    quoteText.textContent = curQuote;
+    // splits string into span of chars
+    curQuote.split('').forEach(char => {
+        const span = document.createElement('span');        
+        span.innerText = char;
+        quoteText.append(span);
+    })
 
+    // increments quote num or cycles to start
     if (quoteNum < numQuotes.length-1) {
         quoteNum++;
     }
@@ -95,14 +104,58 @@ function updateQuote() {
 }
 
 function processCurrentText() {
+    // grabs current text from textbox and splits it for comparision
+    curInput = inputArea.val();
+    curInputArr = curInput.split('');
 
+    // updates variables after each char typed
+    charTyped++;
+    errors = 0;
+
+    // stuffs quote span into an array
+    quoteSpanArr = quoteText.toArray();
+
+    // compares arrays to each other
+    quoteSpanArr.forEach((char, index) => {
+        let typeChar = curInputArr[index];
+
+        if (typeChar == null) {
+            char.classList.remove("correct");
+            char.classList.remove("incorrect");
+        }
+
+        else if (typeChar === char.innerText) {
+            char.classList.add("correct");
+            char.classList.remove("incorrect");
+        }
+
+        else {
+            char.classList.add("incorrect");
+            char.classList.remove("correct");
+        }
+    });
+
+    // update num of errors
+    errorText.text(numErrors+errors);
+
+    // update accuracy
+    let correctChar = (charTyped - (numErrors+errors));
+    let accur = ((correctChar/charTyped)*100);
+    accuracyText.text(Math.round(accur))
+
+    // check to see if need to update quote
+    if (curInput.length == curQuote.length) {
+        updateQuote();
+        numErrors += errors;
+        inputArea.text("");
+    }
 }
 
 function updateTimer() {
     if (timeLeft > 0) {
         timeLeft--; // decreases current time
         timePassed++; // increases time elapsed
-        timerText.textContent = timeLeft;
+        timerText.text(timeLeft);
     }
 
     else {
@@ -115,24 +168,57 @@ function startGame() {
     timer = setInterval(updateTimer, 1000); // sets interval to seconds
 }
 
+function resetValues() {
+    timeLeft = TIME_LIMIT;
+    timePassed = 0;
+    errors = 0;
+    numErrors = 0;
+    accuracy = 0;
+    charTyped = 0;
+    quoteNum = 0;
+    inputArea.prop("disabled", false);  // re enables typing
+
+    inputArea.text("");
+    quoteText.text("Click on the area below to start the game.");
+    accuracyText.text("100");
+    errorText.text("0");
+    restartbtn.style.display="none";
+    nextlvlbtn.style.display="none";
+}
+
 function finishGame() {
     clearInterval(timer); // stops the timer
-    inputArea.disabled = true; // disables typing
-    quoteText.textContent = "You rescued a bee! Click on Next Level to move on."
+    inputArea.prop("disabled", true);  // disables typing
 
-    nextlvlbtn.style.display = "block";
+    if (accuracy >= 70) {
+        quoteText.text("You rescued a bee! Click on Next Level to move on.");
+        nextlvlbtn.style.display = "block";
+    }
 
-    wpm = Math.round((((characterTyped / 5) / timePassed) * 60));
-    wpmText.textContent = wpm;
+    else {
+        quoteText.text("Need 70% accuracy to rescue the bee, press restart to retry!");
+        restartbtn.style.display = "block";
+    }
+
+    wpm = Math.round((((charTyped / 5) / timePassed) * 60));
+    wpmText.text(wpm);
 
     wpmGroup.style.display = "block";
 }
 
-$("#input-area").click(function(){
+$("#input-area").focus(function(){
     startGame();
+ })
+
+$("#input-area").on("input", function(){
+    processCurrentText();
  })
 
 $("#nextlvlbtn").click(function(){
     //window.location.href='pages/bee-select.html';
 	window.location.href='pages/level2.html';
+ })
+
+ $("#restartbtn").click(function(){
+	resetValues();
  })
